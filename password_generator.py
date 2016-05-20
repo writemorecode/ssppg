825075e0662ea80f121a451a4d49988b46943f7f
writemorecode <gustav.ae.karlsson@gmail.com>
2016-05-21 06:48:13 +1000

    Added checking for OS (program cannot run on Windows)

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,20 @@
 """ 
 ssppg - A simple and secure Python password generator. 
-TODO: Look into cryptographically secure PRNG.
 """
+# TODO: Look into cryptographically secure PRNG.
+# TODO: Implement "weight" for different kinds of characters,
+#       if you want more numbers than letters for example.
 
+import platform
 import string
 import random
 import sys
+
+def pre_run():
+    if platform.system() == "Windows":
+        raise OSError("This program only works on Linux systems.")
+
+    # TODO: Check if entropy_avail is low
 
 def generate_password(length, chars):
     password = ""
@@ -16,11 +25,13 @@ def generate_password(length, chars):
     return password
         
 def main():
+    pre_run()
+
     shuffle_iterations = 100
     password_len = 50
 
     numbers = [str(i) for i in range(0,10)]
-    all_chars = list(string.ascii_letters) + numbers
+    all_chars = list(string.ascii_letters + string.punctuation) + numbers
 
     for i in range(shuffle_iterations):
         random.shuffle(all_chars)
