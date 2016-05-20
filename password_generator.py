6a852e4580b57ee8b97a83d06c81a3278f3ef18c
writemorecode <gustav.ae.karlsson@gmail.com>
2016-05-21 06:08:39 +1000

    Initial commit

---
+++ password_generator.py
@@ -0,0 +1,32 @@
+""" 
+ssppg - A simple and secure Python password generator. 
+TODO: Look into cryptographically secure PRNG.
+"""
+
+import string
+import random
+import sys
+
+def generate_password(length, chars):
+    password = ""
+    for i in range(length):
+        c = random.choice(chars)
+        password += c
+
+    return password
+        
+def main():
+    shuffle_iterations = 100
+    password_len = 50
+
+    numbers = [str(i) for i in range(0,10)]
+    all_chars = list(string.ascii_letters) + numbers
+
+    for i in range(shuffle_iterations):
+        random.shuffle(all_chars)
+
+    password = generate_password(password_len, all_chars)
+    print(password)
+
+if __name__ == '__main__':
+    main()
