b6ba866682dbfd8ea018ab0d88e3ae9b2f12cf96
writemorecode <gustav.ae.karlsson@gmail.com>
2017-02-08 20:15:38 +0100

    Program now has the command switches '--symbols' and '--no-symbols' to disable and enable symbols in the generated password.

--- password_generator.py
+++ password_generator.py
@@ -8,6 +8,7 @@ import string
 import random
 import sys
 
+
 def generate_password(length, chars, shuffle_iterations):
     """
     Generates a secure password
@@ -33,19 +34,30 @@ def generate_password(length, chars, shuffle_iterations):
         password += c
 
     return password
-        
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--length", help="How long the generated password should be.", type=int, required=True)
+
+    symbols_group = parser.add_mutually_exclusive_group()
+    symbols_group.add_argument('--symbols', action='store_true', help='Generates passwords with symbols.')
+    symbols_group.add_argument('--no-symbols', action='store_false', help='Generates passwords without symbols.')
+
     args = parser.parse_args()
 
     shuffle_iterations = 100
     password_len = args.length
 
-    numbers = [str(i) for i in range(0,10)]
-    all_chars = list(string.ascii_letters + string.punctuation) + numbers
-    password = generate_password(password_len, all_chars, shuffle_iterations)
+    numbers = [str(i) for i in range(0, 10)]
+
+    if args.symbols:
+        chars = list(string.ascii_letters + string.punctuation) + numbers
+    else:
+        chars = list(string.ascii_letters) + numbers
+    password = generate_password(password_len, chars, shuffle_iterations)
     print(password)
+
 
 if __name__ == '__main__':
     main()
